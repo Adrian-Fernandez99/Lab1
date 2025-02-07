512f49550aeb1f27a1e902250fb7115ddc4b6b46
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2025-02-07 17:33:41 -0600

    Inicio
    Se utiliza el prelab como inicio

---
+++ Laboratorio1/Laboratorio1/main.asm
@@ -0,0 +1,86 @@
+/*
+
+	Laboratorio1.asm
+
+Created: 2/7/2025 5:10:10 PM
+Author : Adrián Fernández
+Descripción:
+	Se realiza dos contadores binario de 4 bits.
+	El conteo es visto por medio de LEDs en la protoboard.
+	Se usan pushbuttons para el incremento y decrecimiento 
+	de los valores.
+*/
+
+
+// Configurar la pila
+.include "M328PDEF.inc"
+.cseg
+.org 0x0000
+
+
+// Configurar el MCU
+LDI R16, LOW(RAMEND)
+OUT SPL, R16
+LDI R16, HIGH(RAMEND)
+OUT SPH, R16
+
+SETUP:
+// Configurar pines de entrada y salida (DDRx, PORTx, PINx)
+// PORTD como entrada con pull-up habilitado
+	LDI		R16, 0x00
+	OUT		DDRD, R16		// Setear puerto D como entrada
+	LDI		R16, 0xFF
+	OUT		PORTD, R16		// Habilitar pull-ups en puerto D
+
+// PORTB como salida inicialmente encendido
+	LDI		R16, 0xFF
+	OUT		DDRB, R16		// Setear puerto B como salida
+
+// Realizar variables
+	LDI		R17, 0xFF
+	LDI		R19, 0x00
+
+	CONTADOR:
+	IN		R16, PIND		// Leemos la entrada del puerto D
+	CP		R17, R16		// Comparamos para ver si hubo cambio de estado
+	BREQ	CONTADOR		// Si no hubo cambio regresamos
+	CALL	DELAY			// Si hubo cambio esperamos para evitar rebote
+	IN		R16, PIND		// Nos aseguramos que la elctura haya sido correcta
+	CP		R17, R16
+	BREQ	CONTADOR
+	MOV		R17, R16		// Generamos nuevo estado antiguo
+
+// Establecemos el bit 2 como botón de suma y el bit 3 como resta.
+	SBRS	R16, 2			// Revisamos que el bit de suma este encendido	
+	CALL	SUMA
+	SBRS	R16, 3			// Revisamos que el bit de resta este encendido	
+	DEC		R19
+	OUT		PINB, R19
+	RJMP	CONTADOR
+
+// Sub-rutina (no de interrupción)
+DELAY:
+	LDI		R18, 0xFF
+SUB_DELAY1:
+	DEC		R18
+	CPI		R18, 0
+	BRNE	SUB_DELAY1
+	LDI		R18, 0xFF
+SUB_DELAY2:
+	DEC		R18
+	CPI		R18, 0
+	BRNE	SUB_DELAY2
+	LDI		R18, 0xFF
+SUB_DELAY3:
+	DEC		R18
+	CPI		R18, 0
+	BRNE	SUB_DELAY3
+	RET
+
+SUMA:
+	INC		R19
+	SBRC	R19, 4
+	LDI		R19, 0x00
+	RET
+
+// Rutinas de interrupción
